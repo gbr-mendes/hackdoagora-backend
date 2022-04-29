257f17ccd15bddf50a2b4069f804c76064d15d0c
Gabriel <gabrielx165@gmail.com>
2022-04-29 17:59:06 -0300

    Defined update user route

--- routes/auth.js
+++ routes/auth.js
@@ -3,9 +3,11 @@ const authValidator = require("../validators/auth")
 const router = express.Router()
 const authController = require("../controllers/auth")
 const checkAdminFieldOnUserCreation = require("../middlewares/validationMiddlewares").checkAdminFieldOnUserCreation
+const upload = require("../utils/multer")
 
 router.post("/register",checkAdminFieldOnUserCreation, authController.createUser)
 router.post("/login", authController.loginUser)
+router.patch("/me",checkAdminFieldOnUserCreation, authValidator.verifyTokenMiddleware, upload.single("image"), authController.updateUser)
 router.get("/me",authValidator.verifyTokenMiddleware, authController.userProfile)
 
 module.exports = router
