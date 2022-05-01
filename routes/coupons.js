639aa2ec8f37c91931673bc79655d0cb41f3b2c3
Eliezer Lobaton <eliezerlobaton39@gmail.com>
2022-05-01 11:37:33 -0300

    coupons routes & controller

--- routes/coupons.js
+++ routes/coupons.js
@@ -1,15 +1,8 @@
 const express = require("express")
 const router = express.Router()
-<<<<<<< HEAD
-const authValidator = require("../validators/auth")
 const couponsController = require("../controllers/coupons")
 
 
-router.get("/", authValidator.verifyTokenMiddleware, couponsController.listcoupons)
-=======
-const controller = require("../controllers/coupon")
-
-router.get("/", controller.listCoupons)
->>>>>>> gabriel
+router.get("/", couponsController.listcoupons)
 
 module.exports = router
